12b045a928fe9d23e7d5327d9302b6995e2a1484
ET CYBERAGENT <efimenkotimofej2@gmail.com>
2026-06-12 14:12:32 +0000

    add description

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-const {app, browserWindow} = require('electron')
+const { app, browserWindow } = require("electron");
 
 function createWindow() {
-    const win = browserWindow({
-        width: 400,
-        height: 300
-    })
-    app.loadFile('index.html')
+  const win = browserWindow({
+    width: 400,
+    height: 500,
+  });
+  win.setMenuBarVisibility(false);
+  win.setTitle("Калькулятор");
+  app.loadFile("index.html");
 }
 
-app.whenReady().then(() => app.createWindow())
-app.on('when-window-close', () => app.quit())
+app.whenReady().then(() => app.createWindow());
+app.on("when-window-close", () => app.quit());
